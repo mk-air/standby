d47107d2fe1f1eea2b287ac892cca48b77fee82b
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-24 01:20:51 +0900

    feat : "init"

---
+++ src/test/java/com/example/work_space/WorkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.work_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
