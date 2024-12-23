75ebbeb7af31faae8cb28c4afd77b13be877bcc3
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-24 01:00:48 +0900

    feat : "GlobalConstants" - 역할 상수 처리

---
+++ src/main/java/com/example/work_space/constants/GlobalConstants.java
@@ -0,0 +1,8 @@
+package com.example.work_space.constants;
+
+import com.example.work_space.member.type.Role;
+
+public abstract class GlobalConstants {
+    public static final String USER_AUTH = "USER_AUTH";
+    public static final String ADMIN_AUTH = "ADMIN_AUTH";
+}
