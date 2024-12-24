14a66d6558674bcef61bf1b728f5ce567cd25cc6
Minsang <minsang1233@gmail.com>
2024-12-25 01:21:41 +0900

    feat : "ListResponseDto" - DTO

---
+++ src/main/java/com/example/work_space/list/dto/ListResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.work_space.list.dto;
+
+import com.example.work_space.list.entity.List;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class ListResponseDto {
+
+    private Long boardId; // 보드 ID
+    private Long id;
+    private String title; // 리스트 제목
+    private Long seq; // 리스트 순서
+    private LocalDateTime createdAt;
+
+    public ListResponseDto(Long boardId, Long id, String title, Long seq, LocalDateTime createdAt) {
+        this.boardId = boardId;
+        this.id = id;
+        this.title = title;
+        this.seq = seq;
+        this.createdAt = createdAt;
+    }
+
+    public ListResponseDto(List list) {
+        this.boardId = list.getBoard().getId();
+        this.id = list.getId();
+        this.title = list.getTitle();
+        this.seq = list.getSeq();
+        this.createdAt = list.getCreateAt();
+    }
+}
