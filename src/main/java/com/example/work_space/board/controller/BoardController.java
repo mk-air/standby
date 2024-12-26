0a00d0f53b8628ce77c42d9c86c03965a4eca843
Minsang <minsang1233@gmail.com>
2024-12-26 20:15:54 +0900

    feat : "BoardController" - API 변경 및 조회 수정

--- src/main/java/com/example/work_space/board/controller/BoardController.java
+++ src/main/java/com/example/work_space/board/controller/BoardController.java
@@ -1,11 +1,9 @@
 package com.example.work_space.board.controller;
 
-import com.example.work_space.board.dto.BoardRequestDto;
-import com.example.work_space.board.dto.BoardResponseDto;
-import com.example.work_space.board.dto.BoardUpdateRequestDto;
-import com.example.work_space.board.dto.BoardUpdateResponseDto;
+import com.example.work_space.board.dto.*;
 import com.example.work_space.board.service.BoardService;
 import com.example.work_space.constants.CommonResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,27 +21,28 @@ public class BoardController {
 
     @PostMapping
     public ResponseEntity<CommonResponse<BoardResponseDto>> createBoard(
-            @RequestBody BoardRequestDto requestDto) {
+            @Valid @RequestBody BoardRequestDto requestDto) {
         BoardResponseDto board = boardService.createBoard(requestDto);
         return ResponseEntity.ok(new CommonResponse<>("보드 생성", board));
     }
 
     // 단건 조회
-    @GetMapping("/{boardId}")
-    public ResponseEntity<CommonResponse<BoardResponseDto>> getBoard(
-            @PathVariable Long boardId) {
-        BoardResponseDto board = boardService.getBoard(boardId);
+    @GetMapping("/{boardId}/members/{memberId}")
+    public ResponseEntity<CommonResponse<BoardDetailResponseDto>> getBoard(
+            @PathVariable Long boardId, @PathVariable Long memberId) {
+        BoardDetailResponseDto board = boardService.getBoard(boardId, memberId);
         return ResponseEntity.ok(new CommonResponse<>("보드 조회 성공", board));
     }
 
     // 다건 조회
-    @GetMapping
-    public ResponseEntity<CommonResponse<List<BoardResponseDto>>> getAllBoards() {
-        List<BoardResponseDto> boards = boardService.getBoards();
+    @GetMapping("/{workSpaceId}")
+    public ResponseEntity<CommonResponse<List<BoardResponseDto>>> getAllBoards(
+            @PathVariable Long workSpaceId
+    ) {
+        List<BoardResponseDto> boards = boardService.getBoards(workSpaceId);
         return ResponseEntity.ok(new CommonResponse<>("보드 조회 성공", boards));
     }
 
-    // 수정
     @PutMapping("/{boardId}")
     public ResponseEntity<CommonResponse<BoardUpdateResponseDto>> updateBoard(
             @PathVariable Long boardId, @RequestBody BoardUpdateRequestDto requestDto) {
@@ -51,10 +50,10 @@ public class BoardController {
         return ResponseEntity.ok(new CommonResponse<>("보드 수정 성공", updateBoard));
     }
 
-    // 삭제
     @DeleteMapping("/{boardId}")
-    public ResponseEntity<CommonResponse<Void>> deleteBoard(@PathVariable Long boardId) {
-        boardService.deleteBoard(boardId);
+    public ResponseEntity<CommonResponse<Void>> deleteBoard(
+            @PathVariable Long boardId, @RequestParam Long memberId) {
+        boardService.deleteBoard(boardId, memberId);
         return ResponseEntity.ok(new CommonResponse<>("보드 삭제 완료"));
     }
 
