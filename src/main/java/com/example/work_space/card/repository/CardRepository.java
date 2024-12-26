1bfacd51e528e871a4e29ad31fbba1cd5cb51bd6
dmsdmst <wtgda98@gmail.com>
2024-12-26 10:08:10 +0900

    feat:CardRepository

--- src/main/java/com/example/work_space/card/repository/CardRepository.java
+++ src/main/java/com/example/work_space/card/repository/CardRepository.java
@@ -1,7 +1,18 @@
 package com.example.work_space.card.repository;
 
 import com.example.work_space.card.entity.Card;
+import com.example.work_space.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findAllByTitle(String searchWord);
+
+    List<Card> findAllByContents(String searchWord);
+
+    List<Card> findAllByDeadline(Date deadline);
+
+    List<Card> findAllByMember(Member member);
 }
