4620ff623876016c77c1d5271ad1e3f3d49e28ce
Minsang <minsang1233@gmail.com>
2024-12-26 20:18:46 +0900

    refactor : "BoardDto" - BoardDetailResponseDto 추가

--- src/main/java/com/example/work_space/board/dto/BoardUpdateRequestDto.java
+++ src/main/java/com/example/work_space/board/dto/BoardUpdateRequestDto.java
@@ -5,16 +5,18 @@ import lombok.Getter;
 @Getter
 public class BoardUpdateRequestDto {
     private Long workspaceId; // 워크스페이스 ID
+    private Long memberId;
     private String title; // 보드 이름
     private String color; // 보드 배경색
     private String img; // 보드 이미지
     private String info; // 보드 설명
 
-    private BoardUpdateRequestDto(String title, String color, String img, String info, Long workspaceId) {
+    private BoardUpdateRequestDto(String title, String color, String img, String info, Long workspaceId, Long memberId) {
         this.title = title;
         this.color = color;
         this.img = img;
         this.info = info;
         this.workspaceId = workspaceId;
+        this.memberId = memberId;
     }
 }
