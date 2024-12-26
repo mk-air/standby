740c1a37b7caa9d14220a506f07f5e2ca7db58a6
dmsdmst <wtgda98@gmail.com>
2024-12-26 16:10:15 +0900

    refactor:CardRequestDto

--- src/main/java/com/example/work_space/card/dto/CardRequestDto.java
+++ src/main/java/com/example/work_space/card/dto/CardRequestDto.java
@@ -9,12 +9,14 @@ public class CardRequestDto {
     private String contents;
     private String deadline;
     private String member; // member email
+    private Long listId;
 
-    public CardRequestDto(String title, String contents, String deadline, String member) {
+    public CardRequestDto(String title, String contents, String deadline, String member, Long listId) {
         this.title = title;
         this.contents = contents;
         this.deadline = deadline;
         this.member = member;
+        this.listId = listId;
     }
 
 }
