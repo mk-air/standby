b667ac406317266716b5698375d450251fc7283b
Minsang <minsang1233@gmail.com>
2024-12-26 20:21:51 +0900

    feat : "BoardService" - BoardService & BoardServiceImpl

---
+++ src/main/java/com/example/work_space/board/service/BoardServiceImpl.java
@@ -0,0 +1,118 @@
+package com.example.work_space.board.service;
+
+import com.example.work_space.board.dto.*;
+import com.example.work_space.board.entity.Board;
+import com.example.work_space.board.repository.BoardRepository;
+import com.example.work_space.workspace.entity.WorkSpace;
+import com.example.work_space.workspace.repository.WorkSpaceRepository;
+import com.example.work_space.workspace.type.WorkSpaceRole;
+import com.example.work_space.workspace_member.entity.WorkSpaceMember;
+import com.example.work_space.workspace_member.repository.WorkSpaceMemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+    private final WorkSpaceRepository workSpaceRepository;
+    private final WorkSpaceMemberRepository workSpaceMemberRepository;
+
+    public BoardServiceImpl(BoardRepository boardRepository, WorkSpaceRepository workSpaceRepository, WorkSpaceMemberRepository workSpaceMemberRepository) {
+        this.boardRepository = boardRepository;
+        this.workSpaceRepository = workSpaceRepository;
+        this.workSpaceMemberRepository = workSpaceMemberRepository;
+    }
+
+    @Transactional
+    @Override
+    public BoardResponseDto createBoard(BoardRequestDto requestDto) {
+        WorkSpace workSpace = workSpaceRepository.findById(requestDto.getWorkspaceId()).orElseThrow();
+        WorkSpaceMember workSpaceMember = workSpaceMemberRepository.findByWorkSpaceIdAndMemberId(requestDto.getWorkspaceId(),requestDto.getMemberId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 워크 스페이스 회원을 찾을 수 없습니다."));
+
+        validationOfRole(workSpaceMember);
+
+        Board board = Board.builder()
+                .workSpace(workSpace)
+                .title(requestDto.getTitle())
+                .color(requestDto.getColor())
+                .img(requestDto.getImg())
+                .info(requestDto.getInfo())
+                .build();
+        Board savedBoard = boardRepository.save(board);
+
+        return new BoardResponseDto(savedBoard);
+
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public BoardDetailResponseDto getBoard(Long boardId, Long memberId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 보드를 찾을 수 없습니다."));
+
+        WorkSpaceMember workSpaceMember = workSpaceMemberRepository.findByWorkSpaceIdAndMemberId(board.getWorkSpace().getId(), memberId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 워크 스페이스 회원을 찾을 수 없습니다."));
+
+        validationOfRole(workSpaceMember);
+
+        return new BoardDetailResponseDto(board);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<BoardResponseDto> getBoards(Long workSpaceId) {
+        List<Board> boardList = boardRepository.findAllByWorkSpaceId(workSpaceId);
+
+        return boardList.stream().map(BoardResponseDto::new).toList();
+    }
+
+    @Transactional
+    @Override
+    public BoardUpdateResponseDto updateBoard(Long boardId, BoardUpdateRequestDto updateRequestDto) {
+        WorkSpaceMember workSpaceMember = workSpaceMemberRepository.findByWorkSpaceIdAndMemberId(updateRequestDto.getWorkspaceId(),updateRequestDto.getMemberId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 워크 스페이스 회원을 찾을 수 없습니다."));
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 보드를 찾을 수 없습니다. ID: " + boardId));
+
+        validationOfRole(workSpaceMember);
+
+        // 엔티티 업데이트
+        board.updateBoard(
+                updateRequestDto.getTitle(),
+                updateRequestDto.getColor(),
+                updateRequestDto.getImg(),
+                updateRequestDto.getInfo()
+        );
+
+        // 수정된 엔티티 저장 후 응답 DTO 반환
+        Board updateBoard = boardRepository.save(board);
+        return new BoardUpdateResponseDto(updateBoard);
+    }
+
+    @Transactional
+    @Override
+    public void deleteBoard(Long boardId, Long memberId) {
+        // 보드 존재 확인
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 보드가 존재하지 않습니다. ID: " + boardId));
+
+        WorkSpaceMember workSpaceMember = workSpaceMemberRepository.findByWorkSpaceIdAndMemberId(board.getWorkSpace().getId(), memberId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 워크 스페이스 회원을 찾을 수 없습니다."));
+
+        validationOfRole(workSpaceMember);
+
+        // 보드 삭제
+        boardRepository.delete(board);
+    }
+
+    // 권한 확인
+    private static void validationOfRole(WorkSpaceMember workSpaceMember) {
+        if(workSpaceMember.getRole().equals(WorkSpaceRole.READ_ONLY)){
+            throw new IllegalArgumentException("ReadOnly 회원은 작성 할 수 없습니다.");
+        }
+    }
+
+}
