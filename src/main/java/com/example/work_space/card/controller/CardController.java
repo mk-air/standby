baf3a91d3ed514b5c39299f5a1c99e3edaef50a0
dmsdmst <wtgda98@gmail.com>
2024-12-26 18:44:59 +0900

    feat:check authorization

--- src/main/java/com/example/work_space/card/controller/CardController.java
+++ src/main/java/com/example/work_space/card/controller/CardController.java
@@ -1,11 +1,15 @@
 package com.example.work_space.card.controller;
 
+import com.example.work_space.auth.entity.Authentication;
 import com.example.work_space.card.dto.CardDetailResponseDto;
 import com.example.work_space.card.dto.CardRequestDto;
 import com.example.work_space.card.dto.CardResponseDto;
 import com.example.work_space.card.dto.CardSearchRequestDto;
 import com.example.work_space.card.service.CardService;
 import com.example.work_space.constants.CommonResponse;
+import com.example.work_space.constants.GlobalConstants;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,17 +26,25 @@ public class CardController {
     }
 
     @PostMapping
-    public ResponseEntity<CommonResponse<CardResponseDto>> createCard(@RequestBody CardRequestDto requestDto) {
+    public ResponseEntity<CommonResponse<CardResponseDto>> createCard(@RequestBody CardRequestDto requestDto, HttpServletRequest request) {
+        HttpSession session =request.getSession(false);
 
-        CardResponseDto responseDto = cardService.createCard(requestDto);
+        Authentication authentication = (Authentication) session.getAttribute(GlobalConstants.USER_AUTH);
+        Long authId = authentication.getId();
+
+        CardResponseDto responseDto = cardService.createCard(requestDto, authId);
         return ResponseEntity.ok(new CommonResponse<>("카드 생성 완료", responseDto));
     }
 
     @PutMapping("/{cardId}")
     public ResponseEntity<CommonResponse<CardResponseDto>> updateCard(
-            @RequestBody CardRequestDto requestDto, @PathVariable Long cardId) {
+            @RequestBody CardRequestDto requestDto, @PathVariable Long cardId, HttpServletRequest request) {
+        HttpSession session =request.getSession(false);
 
-        CardResponseDto responseDto = cardService.updateCard(requestDto, cardId);
+        Authentication authentication = (Authentication) session.getAttribute(GlobalConstants.USER_AUTH);
+        Long authId = authentication.getId();
+
+        CardResponseDto responseDto = cardService.updateCard(requestDto, cardId, authId);
         return ResponseEntity.ok(new CommonResponse<>("카드 수정 완료", responseDto));
     }
 
@@ -46,8 +58,13 @@ public class CardController {
 
     @DeleteMapping("/cardId")
     public ResponseEntity<CommonResponse<String>> deleteCard(
-            @PathVariable Long cardId) {
-        cardService.deleteCard(cardId);
+            @PathVariable Long cardId, HttpServletRequest request) {
+        HttpSession session =request.getSession(false);
+
+        Authentication authentication = (Authentication) session.getAttribute(GlobalConstants.USER_AUTH);
+        Long authId = authentication.getId();
+
+        cardService.deleteCard(cardId, authId);
         return ResponseEntity.ok(new CommonResponse<>("카드 삭제 완료"));
     }
 
