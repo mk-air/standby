588ce8e4c5186fa5030aa145f05aba6cd900c88f
dmsdmst <wtgda98@gmail.com>
2024-12-26 10:07:40 +0900

    feat:CardController

--- src/main/java/com/example/work_space/card/controller/CardController.java
+++ src/main/java/com/example/work_space/card/controller/CardController.java
@@ -1,11 +1,15 @@
 package com.example.work_space.card.controller;
 
+import com.example.work_space.card.dto.CardDetailResponseDto;
 import com.example.work_space.card.dto.CardRequestDto;
 import com.example.work_space.card.dto.CardResponseDto;
+import com.example.work_space.card.dto.CardSearchRequestDto;
 import com.example.work_space.card.service.CardService;
 import com.example.work_space.constants.CommonResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cards")
@@ -31,6 +35,22 @@ public class CardController {
         CardResponseDto responseDto = cardService.updateCard(requestDto, cardId);
         return ResponseEntity.ok(new CommonResponse<>("카드 수정 완료", responseDto));
     }
+
+    @GetMapping("/{cardId}")
+    public ResponseEntity<CommonResponse<CardDetailResponseDto>> getCard(
+            @PathVariable Long cardId) {
+
+        CardDetailResponseDto responseDto = cardService.getCardDetail(cardId);
+        return ResponseEntity.ok(new CommonResponse<>("카드 상세 조회 완료", responseDto));
+    }
+
+    @DeleteMapping("/cardId")
+    public ResponseEntity<CommonResponse<String>> deleteCard(
+            @PathVariable Long cardId) {
+        cardService.deleteCard(cardId);
+        return ResponseEntity.ok(new CommonResponse<>("카드 삭제 완료"));
+    }
+
 
 
 }
