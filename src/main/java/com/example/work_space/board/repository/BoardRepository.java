d146ec82fc6df81103af0ec9571af0e90131b7e2
Minsang <minsang1233@gmail.com>
2024-12-26 20:20:31 +0900

    refactor : "BoardRepository" - findAllByWorkSpaceId

--- src/main/java/com/example/work_space/board/repository/BoardRepository.java
+++ src/main/java/com/example/work_space/board/repository/BoardRepository.java
@@ -2,11 +2,12 @@ package com.example.work_space.board.repository;
 
 import com.example.work_space.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    List<Board> findById(String title);
+    List<Board> findAllByWorkSpaceId(Long workSpaceId);
 }
